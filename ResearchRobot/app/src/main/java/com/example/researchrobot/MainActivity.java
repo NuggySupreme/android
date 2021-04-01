f87bafa16a297bf9af711caff7b8d04c89c2fd74
NuggySupreme <79227257+NuggySupreme@users.noreply.github.com>
2021-04-01 03:06:43 -0500

    New TCP Fix for App
    Made TCP connection version of Robot App

--- ResearchRobot/app/src/main/java/com/example/researchrobot/MainActivity.java
+++ ResearchRobot/app/src/main/java/com/example/researchrobot/MainActivity.java
@@ -2,234 +2,106 @@ package com.example.researchrobot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TARGET_MAC_ADDRESS = "58:96:1D:BF:D1:72";
-    private static final UUID APP_UUID = UUID.fromString("3ae3c348-888d-11eb-8dcd-0242ac130003");
-    private BluetoothAdapter bluetoothAdapter;
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if(device.getAddress().equalsIgnoreCase(TARGET_MAC_ADDRESS)) {
-                    connectRobot(device);
-                }
-            }
-        }
-    };
-    private BluetoothSocket socket = null;
-    private static ConnectedThread connectedThread;
-
-    private void enableBluetooth() {
-        if(!bluetoothAdapter.isEnabled()) {
-            Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(i, 1);
-        }
-    }
-
-    private void searchForRobot() {
-        boolean robotFound = false;
-        BluetoothDevice robot = null;
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        if(pairedDevices.size() > 0) {
-            for(BluetoothDevice device : pairedDevices) {
-                if(device.getAddress().equalsIgnoreCase(TARGET_MAC_ADDRESS)) {
-                    robotFound = true;
-                    robot = device;
-                    break;
-                }
-            }
-        }
-        if(!robotFound) {
-            bluetoothAdapter.startDiscovery();
-        }
-        if(robot != null) {
-            connectRobot(robot);
-        }
-    }
-
-    public synchronized void connectRobot(BluetoothDevice robot) {
-        ConnectThread connectThread = new ConnectThread(robot);
-        connectThread.start();
-    }
-
-    public void connected(BluetoothSocket socket) {
-        connectedThread = new ConnectedThread(socket);
-        connectedThread.start();
-    }
+    private Thread Thread1 = null; //thread to send connection request to robot
+    private EditText etIP, etMessage; //Input field
+    private TextView tvMessages; //TextView for messages from robot
+    private String SERVER_IP; //Robot IP address
+    private PrintWriter toRobot;
+    private BufferedReader fromRobot;
+    private Socket robotConnection = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(receiver, filter);
 
-        Button search = findViewById(R.id.search);
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchForRobot();
-            }
+        //setup UI elements
+        etIP = findViewById(R.id.etIP);
+        tvMessages = findViewById(R.id.tvMessages);
+        etMessage = findViewById(R.id.etMessage);
+        Button btnSend = findViewById(R.id.btnSend);
+        Button btnConnect = findViewById(R.id.btnConnect);
+
+        //Set click event listeners for buttons
+        btnConnect.setOnClickListener(v -> {
+            tvMessages.setText(""); //clear message TextView
+            SERVER_IP = etIP.getText().toString().trim(); //Get robot IP address and remove leading and trailing whitespace
+            Thread1 = new Thread(new Thread1()); //Start a new connection to the robot
+            Thread1.start();
         });
 
-        enableBluetooth();
-        searchForRobot();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
-                bluetoothAdapter.enable();
+        btnSend.setOnClickListener(v -> {
+            String message = etMessage.getText().toString().trim(); //get message to send to robot and cleanup leading and trailing whitespace
+            if(!message.isEmpty()) {
+                new Thread(new Thread3(message)).start(); //start thread to send message to robot
             }
-        }
+        });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(receiver);
-        connectedThread.cancel();
-        try {
-            socket.close();
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
-
-        public ConnectThread(BluetoothDevice device) {
-            mmDevice = device;
-            BluetoothSocket tmp = null;
-
-            // Get a BluetoothSocket for a connection with the
-            // given BluetoothDevice
-            try {
-                tmp = device.createRfcommSocketToServiceRecord(APP_UUID);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            mmSocket = tmp;
-        }
-
+    private class Thread1 implements Runnable { //Sends a TCP connection to the robot
         public void run() {
-            bluetoothAdapter.cancelDiscovery();
-
-            // Make a connection to the BluetoothSocket
-            try {
-                // This is a blocking call and will only return on a
-                // successful connection or an exception
-                mmSocket.connect();
-            } catch (IOException e) {
-                // Close the socket
+            if(!robotConnection.isConnected()) { //if there isn't already a connection to the robot
                 try {
-                    mmSocket.close();
-                } catch (IOException e2) {
-                    e.printStackTrace();
-                }
-                return;
-            }
-
-            // Start the connected thread
-            connected(mmSocket);
-        }
-
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            // Get the BluetoothSocket input and output streams
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            byte[] buffer = new byte[1024];
-            int bytes;
-
-            // Keep listening to the InputStream while connected
-            while (true) {
-                try {
-                    // Read from the InputStream
-                    bytes = mmInStream.read(buffer);
-                    String input = new String(buffer, 0, bytes);
-                    TextView tv = (TextView)findViewById(R.id.msg);
-                    tv.setText(input);
+                    robotConnection = new Socket(SERVER_IP, R.integer.serverPort); //Send connection request to robot
+                    toRobot = new PrintWriter(robotConnection.getOutputStream(), true); //Get output stream to send stuff to robot and set it to auto flush data
+                    fromRobot = new BufferedReader(new InputStreamReader(robotConnection.getInputStream())); //Get input stream to get stuff from robot
+                    //Update UI element with text from input stream
+                    runOnUiThread(() -> tvMessages.setText("Connected\n"));
+                    new Thread(new Thread2()).start(); //start a thread to handle input from robot
                 } catch (IOException e) {
                     e.printStackTrace();
-                    break;
                 }
             }
         }
+    }
 
-        /**
-         * Write to the connected OutStream.
-         *
-         * @param buffer The bytes to write
-         */
-        public void write(byte[] buffer) {
-            try {
-                mmOutStream.write(buffer);
-                mmOutStream.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
+    private class Thread2 implements Runnable { //Gets input from robot
+        @Override
+        public void run() {
+            while(true) {
+                if(robotConnection.isConnected()) { //if the robot is connected
+                    try {
+                        final String message = fromRobot.readLine(); //get message from robot
+                        if(message != null) { //if the message is not empty
+                            runOnUiThread(() -> {
+                                tvMessages.append("robot: " + message + "\n"); //append message to log
+                            });
+                        }
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                } else { //Otherwise robot connection needs to be restarted
+                    Thread1 = new Thread(new Thread1());
+                    Thread1.start();
+                    return;
+                }
             }
         }
+    }
 
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private class Thread3 implements Runnable {
+        private final String message;
+        Thread3(String message) {
+            this.message = message;
+        }
+        @Override
+        public void run() {
+            toRobot.write(message);
+            runOnUiThread(() -> {
+                tvMessages.append("android: " + message + "\n");
+                etMessage.setText("");
+            });
         }
     }
 }
