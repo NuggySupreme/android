e4a5f6b76fada0647fd4b71ab905ab1e16d8d595
NuggySupreme <79227257+NuggySupreme@users.noreply.github.com>
2021-10-15 10:35:50 -0500

    Updated OpenGL View to use OpenGLES2.0

--- BluetoothRobot/app/src/main/java/com/example/bluetoothrobot/ChartGLSurfaceView.java
+++ BluetoothRobot/app/src/main/java/com/example/bluetoothrobot/ChartGLSurfaceView.java
@@ -4,6 +4,7 @@ package com.example.bluetoothrobot;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 
 public class ChartGLSurfaceView extends GLSurfaceView{
 
@@ -16,13 +17,10 @@ public class ChartGLSurfaceView extends GLSurfaceView{
     public ChartGLSurfaceView(Context context) {
         super(context);
 
+        setEGLContextClientVersion(2);
         setEGLConfigChooser(8, 8, 8, 8, 16, 0);
         this.setZOrderOnTop(true);
         getHolder().setFormat(PixelFormat.TRANSLUCENT);
-
-        // Set the Renderer for drawing on the GLSurfaceView
-        chartRenderer = new ChartGLRenderer(context);
-        setRenderer(chartRenderer);
 
         float j = -7.0f;
         for (int i = 0; i < datapoints.length; i+= 2){
@@ -33,9 +31,13 @@ public class ChartGLSurfaceView extends GLSurfaceView{
         setChartData(datapoints);
 
         // Render the view only when there is a change in the drawing data
-        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        // Set the Renderer for drawing on the GLSurfaceView
         requestRender = new RenderThread();
         requestRender.start();
+
+        chartRenderer = new ChartGLRenderer(context);
+        setRenderer(chartRenderer);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 
     public void setChartData(float[] datapoints) {
@@ -64,6 +66,7 @@ public class ChartGLSurfaceView extends GLSurfaceView{
                     e.printStackTrace();
                 }
             }
+            Log.i("info", "render thread closed");
         }
     }
 }
