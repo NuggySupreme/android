946dd093bc2705173760cc4fbea5462bcfcac476
NuggySupreme <79227257+NuggySupreme@users.noreply.github.com>
2022-03-31 16:19:26 -0500

    last changes before handing project over

--- BluetoothRobot/app/src/main/java/com/example/bluetoothrobot/ChartView.java
+++ BluetoothRobot/app/src/main/java/com/example/bluetoothrobot/ChartView.java
@@ -1,11 +1,27 @@
 package com.example.bluetoothrobot;
 
 import android.content.Context;
-import android.view.View;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
 
+public class ChartView extends GLSurfaceView {
+    private ChartGLRenderer renderer;
 
-public class ChartView extends View {
-    public ChartView(Context context){
+    public ChartView(Context context) {
         super(context);
+
+        setEGLContextClientVersion(3);
+        setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        this.setZOrderOnTop(true);
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
+
+        // Render the view only when there is a change in the drawing data
+        // Set the Renderer for drawing on the GLSurfaceView
+        renderer = new ChartGLRenderer(context);
+        setRenderer(renderer);
+    }
+
+    public void addVertex(float val) {
+        renderer.addVertex(val);
     }
 }
